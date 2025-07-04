c7085fea363b267b2c27652cd30ccf1a97142db1
Vipin Nayak <vipinnayak754@gmail.com>
2025-07-04 17:39:47 +0530

    fix: added root path view

--- mybackend/settings.py
+++ mybackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7uek%u-7-1=@y%(ih1coui1_6!hf9$nliq0id+oggrp2)bmk0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-767c1.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 

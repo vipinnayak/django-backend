8f17d32c507cc14bb0a42281d2dee01deba9f320
Vipin Nayak <vipinnayak754@gmail.com>
2025-07-11 15:28:09 +0530

    Add collected static files for Railway deployment

--- mybackend/settings.py
+++ mybackend/settings.py
@@ -126,8 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Add these lines if not present
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
